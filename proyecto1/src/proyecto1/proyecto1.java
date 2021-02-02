af52415f8cc466dd07645fcb8e15c057f19a5b74
Jorge Caballero <jcaballerod15@gmail.com>
2021-02-02 19:16:45 +0100

    Mi primer commit en eclipse

---
+++ proyecto1/src/proyecto1/proyecto1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package proyecto1;
+
+/**
+ * @author jcaballerod
+ *
+ */
+public class proyecto1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
